0935b9401d91c28f49cc56eeea8ac1d589083006
Marcel Cases i Freixenet <m@rcel.xyz>
2021-06-18 13:38:32 +0200

    Code restructured and simplified

--- main.py
+++ main.py
@@ -7,16 +7,23 @@ June 2021
 """
 
 #%%
-import pandas as pd
-import matplotlib.pyplot as plt
+# Dependencies
+import pandas as pd # for data handling
+import matplotlib.pyplot as plt # for linear plot
+import seaborn as sns # for scatter plot
+from sklearn import linear_model
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+from sklearn.neighbors import KNeighborsRegressor
 
 #%%
+# Read sensor data
 sensor = pd.read_csv("data.csv", sep=';', index_col=0, parse_dates=False)
 
-
-# Intro to Pandas
+# Build dataset
+df = pd.DataFrame({'RefSt': sensor["RefSt"], 'Sensor_O3': sensor["Sensor_O3"], 'Temp': sensor["Temp"], 'RelHum': sensor["RelHum"]})
 
 #%%
+# Intro to Pandas
 # Print first top lines from data
 print(sensor.head(5))
 
@@ -35,70 +42,65 @@ print(Temp_Sensor_O3.head(5))
 
 #%%
 # Simple plot
-Temp_Sensor_O3.plot()
-
-
-# Data observation
+df.plot()
+plt.xticks(rotation=20)
 
 #%%
+# Data observation
 # Plot the ozone (KOhms) and ozone reference data (μgr/m^3) as function of time
-Sensor_O3_RefSt = sensor[["Sensor_O3", "RefSt"]]
+Sensor_O3_RefSt = df[["Sensor_O3", "RefSt"]]
 Sensor_O3_RefSt.plot()
 plt.xticks(rotation=20)
 
 # %%
-# Plot the ozone (KOhms) and ozone reference data (μgr/m^3) as function of time - normalised
-Sensor_O3_RefSt_norm = sensor[["Sensor_O3", "RefSt"]]
-Sensor_O3_RefSt_norm["RefSt"] = 4*Sensor_O3_RefSt_norm["RefSt"]
-Sensor_O3_RefSt_norm.plot()
+# Plot the ozone (KOhms) and ozone reference data (μgr/m^3) as function of time - factor
+Sensor_O3_RefSt_factor = df[["Sensor_O3", "RefSt"]]
+Sensor_O3_RefSt_factor["RefSt"] = 4*Sensor_O3_RefSt_factor["RefSt"]
+Sensor_O3_RefSt_factor.plot()
+plt.xticks(rotation=20)
 
 # %%
 # Raw scatter plot
-df = pd.DataFrame({'RefSt': sensor["RefSt"], 'Sensor_O3': sensor["Sensor_O3"]})
-df.plot.scatter(x = 'Sensor_O3', y = 'RefSt')
+sns.lmplot(x = 'Sensor_O3', y = 'RefSt', data = df, fit_reg=True, line_kws={'color': 'orange'}) 
 
+# %%
+# Normalise sensor data
+def normalize(col):
+    μ = col.mean()
+    σ = col.std()
+    return (col - μ)/σ
+
+df["normRefSt"] = normalize(df["RefSt"])
+df["normSensor_O3"] = normalize(df["Sensor_O3"])
+df["normTemp"] = normalize(df["Temp"])
+df["normRelHum"] = normalize(df["RelHum"])
+
+print(df)
 
 # %%
 # Normalised scatter plot
-μSensor_O3 = sensor["Sensor_O3"].mean()
-σSensor_O3 = sensor["Sensor_O3"].std()
-normSensor_O3 = (sensor["Sensor_O3"] - μSensor_O3)/σSensor_O3
-
-print(μSensor_O3, σSensor_O3, normSensor_O3)
-
-μRefSt = sensor["RefSt"].mean()
-σRefSt = sensor["RefSt"].std()
-normRefSt = (sensor["RefSt"] - μRefSt)/σRefSt
-
-print(μRefSt, σRefSt, normRefSt)
-
-df = pd.DataFrame({'normRefSt': normRefSt, 'normSensor_O3': normSensor_O3})
-df.plot.scatter(x = 'normSensor_O3', y = 'normRefSt')
+sns.lmplot(x = 'normSensor_O3', y = 'normRefSt', data = df, fit_reg=True, line_kws={'color': 'orange'}) 
 
 # %%
 # Temp with respect to Sensor_O3
-df = pd.DataFrame({'Temp': sensor["Temp"], 'Sensor_O3': sensor["Sensor_O3"]})
-df.plot.scatter(x = 'Sensor_O3', y = 'Temp')
+sns.lmplot(x = 'Sensor_O3', y = 'Temp', data = df, fit_reg=True, line_kws={'color': 'orange'}) 
 
 # %%
 # Temp with respect to RefSt
-df = pd.DataFrame({'Temp': sensor["Temp"], 'RefSt': sensor["RefSt"]})
-df.plot.scatter(x = 'RefSt', y = 'Temp')
+sns.lmplot(x = 'RefSt', y = 'Temp', data = df, fit_reg=True, line_kws={'color': 'orange'}) 
 
 # %%
 # RelHum with respect to Sensor_O3
-df = pd.DataFrame({'RelHum': sensor["RelHum"], 'Sensor_O3': sensor["Sensor_O3"]})
-df.plot.scatter(x = 'Sensor_O3', y = 'RelHum')
+sns.lmplot(x = 'Sensor_O3', y = 'RelHum', data = df, fit_reg=True, line_kws={'color': 'orange'}) 
 
 # %%
 # RelHum with respect to RefSt
-df = pd.DataFrame({'RelHum': sensor["RelHum"], 'RefSt': sensor["RefSt"]})
-df.plot.scatter(x = 'RefSt', y = 'RelHum')
+sns.lmplot(x = 'RefSt', y = 'RelHum', data = df, fit_reg=True, line_kws={'color': 'orange'}) 
 
 # %%
+# Data calibration
 # Multiple Linear Regression
-from sklearn import linear_model
-df = pd.DataFrame({'Sensor_O3': sensor["Sensor_O3"], 'RefSt': sensor["RefSt"], 'Temp': sensor["Temp"], 'RelHum': sensor["RelHum"] })
+
 X = df[['Sensor_O3', 'Temp', 'RelHum']]
 Y = df['RefSt']
 regr = linear_model.LinearRegression()
@@ -106,39 +108,28 @@ regr.fit(X, Y)
 print('Intercept: \n', regr.intercept_)
 print('Coefficients: \n', regr.coef_)
 
-# Pred = lambda Sensor_O3, Temp, RelHum: -34.0316709 + 0.15929287*Sensor_O3 + 2.49694134*Temp - 0.02949471*RelHum
-MLR = sensor[["Sensor_O3", "RefSt", "Temp", "RelHum"]]
-MLR["MLR_Pred"] = -34.0316709 + 0.15929287*sensor["Sensor_O3"] + 2.49694134*sensor["Temp"] - 0.02949471*sensor["RelHum"]
-print(MLR)
-MLR_plot = MLR[["RefSt", "MLR_Pred"]]
-MLR_plot.plot()
+df["MLR_Pred"] = -34.0316709 + 0.15929287*df["Sensor_O3"] + 2.49694134*df["Temp"] - 0.02949471*df["RelHum"]
+RefSt_MLR_Pred = df[["RefSt", "MLR_Pred"]]
+RefSt_MLR_Pred.plot()
 plt.xticks(rotation=20)
 
 #%%
-# To plot the regression line over the scatter plot
-import seaborn as sns
-
-df = pd.DataFrame({'RefSt': MLR["RefSt"], 'MLR_Pred': MLR["MLR_Pred"]})
-sns.lmplot(x = 'RefSt', y = 'MLR_Pred', data= df, fit_reg=True, line_kws={'color': 'orange'}) 
+sns.lmplot(x = 'RefSt', y = 'MLR_Pred', data = df, fit_reg=True, line_kws={'color': 'orange'}) 
 
 #%%
-# Loss functions
-from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
-
+# Loss functions definition
 def loss_functions(y_true, y_pred):
     print("Loss functions:")
     print("* R-squared =", r2_score(y_true, y_pred))
     print("* RMSE =", mean_squared_error(y_true, y_pred))
     print("* MAE =", mean_absolute_error(y_true, y_pred))
 
-loss_functions(MLR["RefSt"], MLR["MLR_Pred"])
+#%%
+# MLR loss
+loss_functions(df["RefSt"], df["MLR_Pred"])
 
 # %%
 # K-nearest Neighbor
-from sklearn.neighbors import KNeighborsRegressor
-
-# dataset (X=m^2, y=rental price)
-df = pd.DataFrame({'Sensor_O3': sensor["Sensor_O3"], 'RefSt': sensor["RefSt"], 'Temp': sensor["Temp"], 'RelHum': sensor["RelHum"] })
 X = df[['Sensor_O3', 'Temp', 'RelHum']]
 Y = df['RefSt']
 
@@ -147,19 +138,18 @@ neigh = KNeighborsRegressor(n_neighbors=31)
 neigh.fit(X, Y)
 
 # predict
-print(neigh.predict([[36.3637,21.77,53.97]])) #16
-print(neigh.predict([[544.3303,28.4,35.03]])) #116.5
+df["KNN_Pred"] = neigh.predict(df[['Sensor_O3', 'Temp', 'RelHum']])
+print(df)
 
-KNN = df[["Sensor_O3", "RefSt", "Temp", "RelHum"]]
-KNN["KNN_Pred"] = neigh.predict(df[['Sensor_O3', 'Temp', 'RelHum']])
-print(KNN)
-KNN_plot = KNN[["RefSt", "KNN_Pred"]]
-KNN_plot.plot()
+#plot linear
+RefSt_KNN_Pred = df[["RefSt", "KNN_Pred"]]
+RefSt_KNN_Pred.plot()
 plt.xticks(rotation=20)
 
-df = pd.DataFrame({'RefSt': KNN["RefSt"], 'KNN_Pred': KNN["KNN_Pred"]})
+# plot regression
 sns.lmplot(x = 'RefSt', y = 'KNN_Pred', data= df, fit_reg=True, line_kws={'color': 'orange'}) 
 
-loss_functions(KNN["RefSt"], KNN["KNN_Pred"])
+# KNN loss
+loss_functions(df["RefSt"], df["KNN_Pred"])
 
 # %%
